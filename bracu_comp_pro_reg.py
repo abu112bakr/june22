6d79f4c6410db841b1dec700c8e4293010c94fbb
abu112bakr <abubakarhasnathgame@gmail.com>
2022-05-28 22:29:20 +0600

    trying

---
+++ bracu_comp_pro_reg.py
@@ -0,0 +1,15 @@
+n=int(input("please enter a nymber "))
+sum=0
+for i in range(0,n+1):
+    if i%3==0:
+        sum=sum+i
+print(sum)
+
+
+#for other
+range=int(input("enter number "))
+addition=0
+for i in range(0,range+1):
+    if i%3==0:
+        addition=addition+i
+print(addition)
