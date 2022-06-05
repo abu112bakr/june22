28af183911aab890adc4ff50f2c7e61721d70dd3
abu112bakr <abubakarhasnathgame@gmail.com>
2022-06-05 22:17:53 +0600

    till right shift, 2 digit

--- test.py
+++ test.py
@@ -1,22 +1,23 @@
 def right_shift(A):
-    i=len(A)-1
-    #i=0
-    while(i>0):
-        A[i]=A[i-1]
-        i=i-1
-    A[0]=0
-
+    j=0
+    x=int(input("digit "))
+    while (j<x):
+        i=len(A)-1
+        
+        while(i>0):
+            A[i]=A[i-1]
+            i=i-1
+        A[0]=0
+        j=j+1
 A=[1,2,3,4,5]
 print(A)
 right_shift(A)
 print(A)
-right_shift(A)
-print(A)
-right_shift(A)
-print(A)
-right_shift(A)
-print(A)
-right_shift(A)
-print(A)
-right_shift(A)
-print(A)
+
+
+
+
+
+
+
+
