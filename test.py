2440a42e0c2b9b227118e8c29e5dce348246c12c
abu112bakr <abubakarhasnathgame@gmail.com>
2022-06-13 22:41:56 +0600

    lab 1 submitted as single txt file , 13 june

--- test.py
+++ test.py
@@ -1,15 +1,55 @@
-def find_same(a):
-  for i in range(0,len(a)):
-    print(i)
-    
-    for j in range(i+1,len(a)):
-      print(j,end=" ")
-      if a[i]==a[j]:
-        print()
-        print(a[i]," matches")
-    print()
-a=[1,5,6,8,5]
-a=[1,3,6,8,7]
-a=[1,4,1,6,8,5]
-find_same(a)
+#4
+#Rotate Right k cells 
+# Consider an array named source. Write a method/function named rotateRight( source, k) 
+#that rotates all the elements of the source array to the right by 'k' positions. 
+#You must execute the method by passing an array and number of cells to be shifted. 
+#After calling the method, print the array to show whether the elements have been shifted properly.
+#Example:
+#source=[10,20,30,40,50,60]
+#rotateRight(source,3)
+#After calling rotateRight(source,3), printing the array should give the output as: 
+# [ 40, 50, 60, 10, 20, 30]
 
+#Rak sir method
+def rotateRight( source, k):
+
+  j=0
+  while(j<k):
+    i=len(source)-1
+    temp=source[i]  #new last index is stored
+    while (i>=1):
+      source[i]=source[i-1]
+      i=i-1
+    source[0]=temp  #new first index = saved index
+    j=j+1
+source=[10,20,30,40,50,60]
+print(source)
+rotateRight(source,3)
+print(source)
+print("##########################")
+#DOME BY SIR HIMSELF
+##rotate right LAB-sir     #have to work dynamcally
+def rotate_Right(source,k):  #see phone pic
+  temp_array=[0]*k
+  t = k-1
+  for i in range(len(source)-1,len(source)-1-k,-1):
+    temp_array[t]=source[i]
+    t -= 1
+  #print(temp_array)
+  i=len(source)-1
+  while(i>=k):
+    source[i]=source[i-k]   #new
+    i=i-1
+#  j=0
+#  while(j<k):
+#    source[j]=0
+#    j=j+1
+  j=0
+  while(j<k):
+    source[j]=temp_array[j]
+    j=j+1
+source=[10,20,30,40,50,60]
+print(source)
+rotate_Right(source,3)
+print(source)
+#Done :)
--- test2.py
+++ test2.py
@@ -1,2 +1,23 @@
-for i in range(10,15):
-  print(i)
+#Task 5
+#Remove an element from an array
+# Consider an array named source. Write a method/function named remove( source, size, idx) that removes the element in index idx of the source array. You must execute the method by passing an array, its size and the idx( that is the index of the element to be removed). After calling the method, print the array to show whether the element of that particular index has been removed properly.
+#Example:
+#source=[10,20,30,40,50,0,0]
+#remove(source,5,2)
+#After calling remove(source,5,2) , printing the array should give the output as: 
+# [ 10,20,40,50,0,0,0]
+
+
+def REMOVE( source, size,index):
+  i=index+1
+  while(i<size):
+    source[i-1]=source[i]
+    i+=1
+  #source[len(source)-1]=0
+  source[size-1]=0
+
+source=[ 10,20,30,40,50,0,0 ]
+print(source)
+#shiftLeft(source,1)
+REMOVE(source,5,2)
+print(source)
