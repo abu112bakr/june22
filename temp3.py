edaf264bf54a0aa61dce4ca32e634be56efccfe1
abu112bakr <abubakarhasnathgame@gmail.com>
2022-07-04 22:17:50 +0600

    dummy headed doubly linked list Lab 4 done and submitted

--- temp3.py
+++ temp3.py
@@ -1,17 +1,27 @@
-#    0 1 2 3 4
-lin_arr=[1,2,3,4,5]
-st=3
-sz=5
+#        0 1 2 3 4
+cir_arr=[4,0,1,2,3]
 
-cir_arr=[0]*len(lin_arr)
-temp=st
-i=0
-while(i<len(lin_arr)):
-  cir_arr[temp]=lin_arr[i]
-  temp=(temp+1)%len(cir_arr)
-  i=i+1
+def insert(cir_arr,st,sz,elem,idx):
 
+  lastposition=(st+sz-1)%len(cir_arr)
+  idx=idx
+  n_item_toshift=sz-idx
+  temp=lastposition
+  for j in range(0,n_item_toshift):
+    cir_arr[(temp+1)%len(cir_arr)] = cir_arr[temp]
+    temp=temp-1
+    if temp<0:
+      temp=len(cir_arr)-1
+  cir_arr[idx]=elem
+insert(cir_arr,2,4,100,3)
 print(cir_arr)
+
+
+
+
+
+
+
 
 
 
