9750e8015875a63c3d743ae16a7a77f620a04a43
Abu Bakar Hasnath <abubakarhasnathgame@gmail.com>
2022-08-26 00:12:27 +0600

    insertion sort.py like bux

--- cse220/insertion_sort.py
+++ cse220/insertion_sort.py
@@ -1,11 +1,14 @@
+#insertion sort bux code
 A=[5,1,9,-3]
+#  j i
 for i in range(1,len(A)):
-    t=A[i]
+    key=A[i]
     j=i-1
-    while(j==0 and A[j]>t):
+    while(j>=0 and key<A[j]):
         A[j+1]=A[j]
         j=j-1
-    A[j+1]=t
+    A[j+1]=key
+
 print(A)
 
 
